f5fb7ed708bc1c82332c385cfd364c4616889d38
zukui <71074389+zukui1984@users.noreply.github.com>
2021-02-01 15:21:57 +0100

    serverless task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
+
+// import { getEvents, checkToken } from "./api";
+
 import './App.css';
+
 
 function App() {
   return (
@@ -21,5 +25,19 @@ function App() {
     </div>
   );
 }
+
+// async componentDidMount() => { 
+//   const accessToken =localStorage.getItem("access_token");   
+//   const validToken = accessToken !== null  ? awaitcheckToken(accessToken) : false;    
+//   this.setState({ tokenCheck: validToken }); 
+//      if(validToken === true) this.updateEvents() 
+//  const searchParams = newURLSearchParams(window.location.search); 
+//  const code = searchParams.get("code");   
+//  this.mounted = true;    
+//  if (code && this.mounted === true && validToken=== false) {
+//      this.setState({tokenCheck:false });   
+//         this.updateEvents()
+//     } 
+//    }
 
 export default App;
