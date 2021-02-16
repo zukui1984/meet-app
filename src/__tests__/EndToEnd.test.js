ed186b05318018db6425ed3df180bd2c0b99abeb
zukui <71074389+zukui1984@users.noreply.github.com>
2021-02-16 10:55:24 +0100

    correction EndtoEnd

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,35 +1,40 @@
 import puppeteer from "puppeteer";
 
-describe('show/hide an event details', () => {
+describe("show/hide an event details", () => {
   let browser;
   let page;
-  beforeAll(async () => {
   jest.setTimeout(30000);
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 250,
+      ignoreDefaultArgs: ["--disable-extensions"],
+    });
+
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
   });
 
   afterAll(() => {
-   browser.close();
+    browser.close();
   });
 
- test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.event .event__Details');
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .description");
     expect(eventDetails).toBeNull();
   });
 
-  test('User can expand an event to see its details', async () => {
-    await page.click('.event .details-btn');
-    const eventDetails = await page.$('.event .event__Details');
+  test("User can expand an event to see its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .description");
     expect(eventDetails).toBeDefined();
   });
 
-  test('User can collapse an event to hide its details', async () => {
-    await page.click('.event .details-btn');
-    const eventDetails = await page.$('.event .event__Details');
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .description");
     expect(eventDetails).toBeNull();
   });
 });
-
